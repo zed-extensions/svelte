2b831580cc053ce40c2c614ba5f758ad931335e0
MrSubidubi <dev@bahn.sh>
2025-08-28 20:32:25 +0200

    Improve style script indents

--- languages/svelte/indents.scm
+++ languages/svelte/indents.scm
@@ -11,4 +11,10 @@
   (element
     (start_tag) @start
     [(end_tag) (erroneous_end_tag)]? @end)
+  (script_element
+    (start_tag) @start
+    [(end_tag) (erroneous_end_tag)]? @end)
+  (style_element
+    (start_tag) @start
+    [(end_tag) (erroneous_end_tag)]? @end)
 ] @indent
