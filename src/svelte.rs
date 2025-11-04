5f3610e1176801fb3cbcf88a0c58967fce1e5922
realfakenerd <lucas210898@hotmail.com>
2025-11-04 10:32:59 -0300

    refactor: remove custom runtime detection and usage
    The custom `Runtime` logic, which attempted to detect and use Bun, is
    removed.

--- src/svelte.rs
+++ src/svelte.rs
@@ -1,8 +1,12 @@
-mod runtime;
-
 use std::{collections::HashSet, env, path::PathBuf};
-use zed_extension_api::{self as zed, Result, serde_json};
-use runtime::Runtime;
+use zed_extension_api::{self as zed, serde_json, Result};
+
+struct SvelteExtension {
+    installed: HashSet<String>,
+}
+
+const PACKAGE_NAME: &str = "svelte-language-server";
+const TS_PLUGIN_PACKAGE_NAME: &str = "typescript-svelte-plugin";
 
 fn get_package_path(package_name: &str) -> Result<PathBuf> {
     let path = env::current_dir()
@@ -12,21 +16,13 @@ fn get_package_path(package_name: &str) -> Result<PathBuf> {
     Ok(path)
 }
 
-struct SvelteExtension {
-    installed: HashSet<String>,
-    runtime: Runtime
-}
-
-const PACKAGE_NAME: &str = "svelte-language-server";
-const TS_PLUGIN_PACKAGE_NAME: &str = "typescript-svelte-plugin";
-
 impl SvelteExtension {
     fn install_package_if_needed(
         &mut self,
         id: &zed::LanguageServerId,
         package_name: &str,
     ) -> Result<()> {
-        let installed_version = self.runtime.installed_package_version(package_name)?;
+        let installed_version = zed::npm_package_installed_version(package_name)?;
 
         // If package is already installed in this session, then we won't reinstall it
         if installed_version.is_some() && self.installed.contains(package_name) {
@@ -38,7 +34,7 @@ impl SvelteExtension {
             &zed::LanguageServerInstallationStatus::CheckingForUpdate,
         );
 
-        let latest_version = self.runtime.latest_package_version(package_name)?;
+        let latest_version = zed::npm_package_latest_version(package_name)?;
 
         if installed_version.as_ref() != Some(&latest_version) {
             println!("Installing {package_name}@{latest_version}...");
@@ -48,7 +44,7 @@ impl SvelteExtension {
                 &zed::LanguageServerInstallationStatus::Downloading,
             );
 
-            if let Err(error) = self.runtime.install_package(package_name, &latest_version){
+            if let Err(error) = zed::npm_install_package(package_name, &latest_version) {
                 // If installation failed, but we don't want to error but rather reuse existing version
                 if installed_version.is_none() {
                     Err(error)?;
@@ -67,7 +63,6 @@ impl zed::Extension for SvelteExtension {
     fn new() -> Self {
         Self {
             installed: HashSet::new(),
-            runtime: Runtime::new()
         }
     }
 
@@ -79,10 +74,19 @@ impl zed::Extension for SvelteExtension {
         self.install_package_if_needed(id, PACKAGE_NAME)?;
         self.install_package_if_needed(id, TS_PLUGIN_PACKAGE_NAME)?;
 
-        let server_path = get_package_path(PACKAGE_NAME)?
-            .join("bin/server.js");
+        let path = get_package_path(PACKAGE_NAME)?
+            .join("bin/server.js")
+            .to_string_lossy()
+            .to_string();
 
-        self.runtime.server_command(&server_path.to_string_lossy())
+        Ok(zed::Command {
+            command: zed::node_binary_path()?,
+            args: vec![
+                path,
+                "--stdio".to_string(),
+            ],
+            env: Default::default(),
+        })
     }
 
     fn language_server_initialization_options(
@@ -90,7 +94,6 @@ impl zed::Extension for SvelteExtension {
         _: &zed::LanguageServerId,
         _: &zed::Worktree,
     ) -> Result<Option<serde_json::Value>> {
-
         let config = serde_json::json!({
           "inlayHints": {
             "parameterNames": {
@@ -132,24 +135,23 @@ impl zed::Extension for SvelteExtension {
         target_id: &zed::LanguageServerId,
         _: &zed::Worktree,
     ) -> Result<Option<serde_json::Value>> {
-        match target_id.as_ref() {
-            "vtsls" => {
-                let plugin_location = get_package_path(TS_PLUGIN_PACKAGE_NAME)?
-                    .to_string_lossy()
-                    .to_string();
 
-                Ok(Some(serde_json::json!({
-                    "vtsls": {
-                        "tsserver": {
-                            "globalPlugins": [{
-                                "name": TS_PLUGIN_PACKAGE_NAME,
-                                "location": plugin_location,
-                                "enableForWorkspaceTypeScriptVersions": true
-                            }]
-                        }
-                    },
-                })))
-            },
+        let plugin_location = get_package_path(TS_PLUGIN_PACKAGE_NAME)?
+            .to_string_lossy()
+            .to_string();
+
+        match target_id.as_ref() {
+            "vtsls" => Ok(Some(serde_json::json!({
+                "vtsls": {
+                    "tsserver": {
+                        "globalPlugins": [{
+                            "name": TS_PLUGIN_PACKAGE_NAME,
+                            "location": plugin_location,
+                            "enableForWorkspaceTypeScriptVersions": true
+                        }]
+                    }
+                },
+            }))),
             _ => Ok(None),
         }
     }
