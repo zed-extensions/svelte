266a5440294a173d9560ce94f4ee44843d38b65f
themixednuts <118081862+themixednuts@users.noreply.github.com>
2026-05-18 08:32:54 -0500

    Update Svelte grammar to tree-sitter-htmlx 0.1.13

--- languages/svelte/brackets.scm
+++ languages/svelte/brackets.scm
@@ -1,7 +1,7 @@
 ("<" @open ">" @close)
 ("{" @open "}" @close)
-("'" @open "'" @close)
-("\"" @open "\"" @close)
+(("'" @open "'" @close) (#set! rainbow.exclude))
+(("\"" @open "\"" @close) (#set! rainbow.exclude))
 ("(" @open ")" @close)
 
 ((element (start_tag) @open [(end_tag) (erroneous_end_tag)] @close) (#set! newline.only))
--- languages/svelte/injections.scm
+++ languages/svelte/injections.scm
@@ -3,18 +3,38 @@
   (start_tag
     (tag_name) @_tag
     (attribute
+      (attribute_name) @_script_attr
       (quoted_attribute_value
         (attribute_value) @_value)))
   (raw_text) @injection.content)
   (#match? @_tag "^[Ss][Cc][Rr][Ii][Pp][Tt]$")
-  (#any-of? @_value "ts" "typescript" "text/typescript")
+  (#any-of? @_script_attr "lang" "type")
+  (#any-of? @_value "ts" "typescript" "text/typescript" "application/typescript")
   (#set! injection.language "typescript"))
+
+; Script elements with explicit JavaScript
+((element
+  (start_tag
+    (tag_name) @_tag
+    (attribute
+      (attribute_name) @_script_attr
+      (quoted_attribute_value
+        (attribute_value) @_value)))
+  (raw_text) @injection.content)
+  (#match? @_tag "^[Ss][Cc][Rr][Ii][Pp][Tt]$")
+  (#any-of? @_script_attr "lang" "type")
+  (#any-of? @_value "js" "javascript" "text/javascript" "application/javascript" "module")
+  (#set! injection.language "javascript"))
 
 ; Script content (JavaScript default)
 ((element
-  (start_tag (tag_name) @_tag)
+  (start_tag
+    (tag_name) @_tag
+    (attribute
+      (attribute_name) @_script_attr)*)
   (raw_text) @injection.content)
-  (#eq? @_tag "script")
+  (#match? @_tag "^[Ss][Cc][Rr][Ii][Pp][Tt]$")
+  (#not-any-of? @_script_attr "lang" "type")
   (#set! injection.language "javascript"))
 
 ; Style with lang="scss"
@@ -22,10 +42,12 @@
   (start_tag
     (tag_name) @_tag
     (attribute
+      (attribute_name) @_lang
       (quoted_attribute_value
         (attribute_value) @_scss)))
   (raw_text) @injection.content)
   (#eq? @_tag "style")
+  (#eq? @_lang "lang")
   (#eq? @_scss "scss")
   (#set! injection.language "scss"))
 
@@ -34,10 +56,12 @@
   (start_tag
     (tag_name) @_tag
     (attribute
+      (attribute_name) @_lang
       (quoted_attribute_value
         (attribute_value) @_sass)))
   (raw_text) @injection.content)
   (#eq? @_tag "style")
+  (#eq? @_lang "lang")
   (#eq? @_sass "sass")
   (#set! injection.language "sass"))
 
@@ -46,12 +70,28 @@
   (start_tag
     (tag_name) @_tag
     (attribute
+      (attribute_name) @_lang
       (quoted_attribute_value
         (attribute_value) @_less)))
   (raw_text) @injection.content)
   (#eq? @_tag "style")
+  (#eq? @_lang "lang")
   (#eq? @_less "less")
   (#set! injection.language "less"))
+
+; Style with explicit CSS
+((element
+  (start_tag
+    (tag_name) @_tag
+    (attribute
+      (attribute_name) @_lang
+      (quoted_attribute_value
+        (attribute_value) @_css)))
+  (raw_text) @injection.content)
+  (#eq? @_tag "style")
+  (#eq? @_lang "lang")
+  (#eq? @_css "css")
+  (#set! injection.language "css"))
 
 ; Style content (CSS default — only when no lang attribute)
 ((element
